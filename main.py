7e0e0879e50f71c6158c92ea2c03681449e3cad0
nnaman123 <sweta.singh21@gmail.com>
2025-07-04 20:36:16 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#Print a sentence
+print("Welcome to the Python world!")
+
+# Print a number
+print(42)
+
+#next line after printing
+print("Hello, World! \n")
+
+#pass multiple arguments to print
+print("Python", "is", "fun!")
+
+#End argument of print statement
+print("This will not end with a newline", end="*")
